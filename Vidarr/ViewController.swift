c4fc234965f175550a8e8143bdad14fc47bfbc4d
Mani <mani1261790@gmail.com>
2026-02-25 21:28:51 +0900

    Initial Commit

---
+++ Vidarr/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Vidarr
+//
+//  Created by 中川誠星 on 2026/02/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ VidarrTests/VidarrTests.swift
@@ -0,0 +1,17 @@
+//
+//  VidarrTests.swift
+//  VidarrTests
+//
+//  Created by 中川誠星 on 2026/02/25.
+//
+
+import Testing
+@testable import Vidarr
+
+struct VidarrTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
